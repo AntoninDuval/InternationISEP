52ce8067675f1b79060a7ad02670532b7751547a
Rikum <antonin.duval@hotmail.fr>
2016-11-25 16:56:23 +0100

    test first servlet

--- src/first/servel/Home.java
+++ src/first/servel/Home.java
@@ -1,15 +1,16 @@
 package first.servel;
 
 import java.io.IOException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Home
- */
+
 @WebServlet("/Home")
 public class Home extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -26,7 +27,14 @@ public class Home extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<HTML>");
+		out.println("<HEAD><TITLE>Our First Servlet</TITLE></HEAD>");
+		out.println("<BODY>");
+		out.println("<H1>Hello World </H1>");
+		out.println("</BODY></HTML>");
+		out.close();
 	}
 
 	/**
